5c3f5eafd16cbc40171eb3a9d0c952ec7e78f941
aashirwadgupta <aash.gupta2@gmail.com>
2020-09-17 22:55:34 +0530

    Correcting discount prices in Java code as well

--- src/main/com/aashirwad/util/ItemPromotionsUtil.java
+++ src/main/com/aashirwad/util/ItemPromotionsUtil.java
@@ -14,7 +14,7 @@ public class ItemPromotionsUtil {
     public static Map<String, PromotionNodeModel> getPromotionData(){
         Map<String, PromotionNodeModel> promotionData = new TreeMap<>();
         promotionData.put("A", new PromotionNodeModel("A", 3, 130, null));
-        promotionData.put("B", new PromotionNodeModel("B", 2, 45, null));
+        promotionData.put("B", new PromotionNodeModel("B", 2, 70, null));
         promotionData.put("C", new PromotionNodeModel("C", 1, 30, null));
         promotionData.put("D", new PromotionNodeModel("D", 1, 30, null));
         promotionData.get("C").setJointDiscountNode(promotionData.get("D"));
