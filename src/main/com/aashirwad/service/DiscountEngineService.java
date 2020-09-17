f4bbe1534a3efd71e2c1d0584dc4de676c513ad4
aashirwadgupta <aash.gupta2@gmail.com>
2020-09-17 22:24:53 +0530

    Adding the alogrithm related notes

--- src/main/com/aashirwad/service/DiscountEngineService.java
+++ src/main/com/aashirwad/service/DiscountEngineService.java
@@ -23,6 +23,32 @@ public class DiscountEngineService {
     public int calculateCartValue(Map<String, Integer> cart){
         Map<String, Integer> itemPriceData = ItemPriceUtil.itemPricing();
         Map<String, PromotionNodeModel> promotionData =ItemPromotionsUtil.getPromotionData();
+
+        Map<String, Integer> cartFinal = new TreeMap<>();
+
+        for(Map.Entry<String, Integer> cartItemEntry: cart.entrySet()){
+            int itemQuantity = cartItemEntry.getValue();
+            String item = cartItemEntry.getKey();
+            PromotionNodeModel currNode = promotionData.get(item);
+            /**
+             * 1. First we will check that if the joinDiscount node is null, that will mean that this will be a solo
+             *    discounted item. If so, then we will check if the quantity is greater or equal to the promotion
+             *    quantity, and accordingly multiply it discounted quantity bundle with the discounted price and the
+             *    remaining with their original price. For Ex - Item1 @ 50 each is 7 in cart and is available at
+             *    3 for 130, then 2 bundle(3 in one bundle) will be evaluated for 130 each and
+             *    remaining one will be priced at 50.
+             * 2. If the associated node is not null, then we will consider this one as a joint promotion.
+             *    In case of joint promotion, if the currentPromotionNode's quantity in the care is more than the
+             *    associatedNode's quantity in the cart, then we will subtract the associated item's quantity,
+             *    with currentNode quantity, and multiply it with promotionalValue. and the remaining quantity 
+             *    with the currentNode's original price.
+             * 3. If in the above scenario, the associatedNode's item quantity, then the calculation get reversed.
+             *    If the quantity, is equal, then any node's promotional value can be multiplied.
+             *
+             * Keep adding this in total and we will get final amount of the cart
+             *
+             */
+        }
         return 0;
     }
 }
